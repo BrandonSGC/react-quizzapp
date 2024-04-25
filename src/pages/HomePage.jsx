a83c7e31655ee6e84e5a2ef141e7bcb0337dad13
BrandonSGC <brangmz20@gmail.com>
2024-04-24 20:20:37 -0600

    General Styles

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,40 @@
+import { ThemeButton } from "../components/";
+import { QuizCard } from "../components/QuizCard";
+
 export const HomePage = () => {
   return (
-    <h1 className="text-center">Hello World</h1>
-  )
-}
+    <>
+      <header className="">
+        <div className="mycontainer">
+          <div className="flex justify-end py-5">
+            <ThemeButton />
+          </div>
+        </div>
+      </header>
+
+      <main className="grid h-full place-items-center">
+        <div className="mycontainer">
+          <div className="grid gap-5 my-10 md:grid-cols-2">
+            {/* Welcome */}
+            <div className="space-y-5">
+              <h1 className="text-4xl text-balance">Welcome to the QuizzApp</h1>
+              <p className="text-slate-500 dark:text-slate-400">
+                Pick a subject to get started!
+              </p>
+            </div>
+
+            {/* List Quizzes */}
+            <div className="grid gap-4">
+              <QuizCard iconURL="/src/assets/html.svg" name="HTML" />
+              <QuizCard iconURL="/src/assets/css.svg" name="CSS" />
+              <QuizCard
+                iconURL="/src/assets/javascript.svg"
+                name="javascript"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
