a9d142e3cfc60d62055af33469a0aaf80b844301
BrandonSGC <brangmz20@gmail.com>
2024-05-01 15:38:05 -0600

    Auth Pages Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-import { HomePage, QuizPage } from "./pages/";
+import { HomePage, LoginPage, QuizPage, RegisterPage } from "./pages/";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
+    <>
+    <ToastContainer />
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/quiz/:id" element={<QuizPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
       <Route path="/*" element={<HomePage />} />
     </Routes>
+    </>
+    
   );
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -36,6 +36,20 @@ export const HomePage = () => {
               <p className="text-xl text-slate-500 dark:text-slate-300">
                 Pick a subject to get started!
               </p>
+              <div className="flex items-center gap-2">
+                <Link
+                  to={"/login"}
+                  className="px-4 py-1 font-medium text-purple-500 bg-white border border-purple-500 rounded shadow hover:bg-gray-100"
+                >
+                  Login
+                </Link>
+                <Link
+                  to="/register"
+                  className="px-4 py-1 font-medium text-white bg-purple-500 rounded shadow hover:bg-purple-600"
+                >
+                  Register
+                </Link>
+              </div>
             </div>
 
             <QuizzesList quizzes={quizzes} />
