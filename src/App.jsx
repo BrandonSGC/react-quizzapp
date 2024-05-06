51fd09d777ff2b31329f86bf8e522d87a07e64d3
BrandonSGC <brangmz20@gmail.com>
2024-05-05 21:54:24 -0600

    User context

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage, LoginPage, QuizPage, RegisterPage } from "./pages/";
 import { ToastContainer } from "react-toastify";
+import { UserProvider } from "./context/UserContext";
 
 function App() {
   return (
-    <>
-    <ToastContainer />
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/quiz/:id" element={<QuizPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/*" element={<HomePage />} />
-    </Routes>
-    </>
-    
+    <UserProvider>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/quiz/:id" element={<QuizPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/*" element={<HomePage />} />
+      </Routes>
+    </UserProvider>
   );
 }
 
---
+++ src/hooks/useUserContext.js
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+
+export const useUserContext = () => {
+  const { user, setUser } = useContext(UserContext);
+
+  return {
+    user,
+    setUser,
+  };
+};
