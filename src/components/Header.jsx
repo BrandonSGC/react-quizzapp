eae793b8e66f4639e65642abe88c7c93c2665996
BrandonSGC <brangmz20@gmail.com>
2024-05-20 15:00:25 -0600

    fix - header navigation button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,22 @@
 import { BackArrowIcon } from "../assets";
 import { ThemeButton } from ".";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const Header = () => {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(-1);
+  }
 
   return (
     <header className="py-5">
       <div className="flex items-center justify-between mycontainer">
         {(pathname !== "/") && (
-          <Link className="size-10" to="/">
+          <div onClick={handleClick} className="size-10">
             <BackArrowIcon />
-          </Link>
+          </div>
         )}
         <div className="w-[100%] size-10 flex justify-end">
           <ThemeButton />
